8f5953790bf812bc4e3e6c85a5e86f37ac27dfe3
tlos3r <isla17092002@gmail.com>
2024-04-04 23:26:05 +0700

    :sparkles: them file homeview

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,6 @@
 import AdminView from './admin/AdminView.vue'
 import DashboardView from './admin/DashboardView.vue'
 import ProductView from './admin/ProductView.vue'
-export { AdminView, DashboardView, ProductView }
+import HomeView from './HomeView.vue'
+
+export { AdminView, DashboardView, ProductView, HomeView }
